6ea9a2a263c25b201299d6c774aa424fcab37f06
SandhyaVelavan <sandyvel275@gmail.com>
2024-02-09 20:35:29 +0530

    building it

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import reactDOM from react-dom/client;
+
+Function Headers(){
+    return(
+    <>
+    <div className="background">
+    <h1>Hello</h1>
+    <p>I'm here to help you</p>
+    </div>
+    </>)
+}
